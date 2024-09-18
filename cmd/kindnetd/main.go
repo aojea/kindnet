1bca5f6470bd1753cb1c77c59b7ab8f0462b3750
Antonio Ojea <aojea@google.com>
2024-09-18 19:37:44 +0000

    enable nat64 by default on ipv6 only clusters
    Change-Id: I689b5ce1a215eb01a91785aba4259d198e9a307b

--- cmd/kindnetd/main.go
+++ cmd/kindnetd/main.go
@@ -21,6 +21,7 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"net/netip"
 	"os"
 	"os/signal"
 	"time"
@@ -75,7 +76,7 @@ func init() {
 	flag.BoolVar(&useBridge, "cni-bridge", false, "If set, enable the CNI bridge plugin (default is the ptp plugin)")
 	flag.BoolVar(&networkpolicies, "network-policy", true, "If set, enable Network Policies (default true)")
 	flag.BoolVar(&dnsCaching, "dns-caching", false, "If set, enable Kubernetes DNS caching (default false)")
-	flag.BoolVar(&nat64, "na64", false, "If set, enable NAT64 using the reserved prefix 64:ff9b::/96 (default false)")
+	flag.BoolVar(&nat64, "nat64", true, "If set, enable NAT64 using the reserved prefix 64:ff9b::/96 on IPv6 only clusters (default true)")
 	flag.StringVar(&hostnameOverride, "hostname-override", "", "If non-empty, will be used as the name of the Node that kube-network-policies is running on. If unset, the node name is assumed to be the same as the node's hostname.")
 	flag.BoolVar(&masquerading, "masquerading", true, "masquerade with the Node IP the cluster to external traffic (default true)")
 	flag.StringVar(&noMasqueradeCIDRs, "no-masquerade-cidr", "", "Comma seperated list of CIDRs that will not be masqueraded.")
@@ -196,8 +197,12 @@ func main() {
 		klog.Info("Skipping ipMasqAgent")
 	}
 
-	// create an nat64 agent
-	if nat64 {
+	// create an nat64 agent if nat64 is enabled and is an IPv6 only cluster
+	ip, err := netip.ParseAddr(podIP)
+	if err != nil {
+		klog.Fatalf("can not parse ip %s : %v", podIP, err)
+	}
+	if nat64 && ip.Is6() {
 		klog.Infof("nat64 traffic")
 		nat64Agent, err := NewNAT64Agent()
 		if err != nil {
