080db58fae7f1bf01c943e3a6f183372e274b22d
Antonio Ojea <aojea@google.com>
2024-12-26 22:29:31 +0000

    test add 300 pods sequentially

--- cmd/cni-kindnet/cni_test.go
+++ cmd/cni-kindnet/cni_test.go
@@ -243,3 +243,125 @@ func TestAddDel(t *testing.T) {
 	}
 	t.Logf("%d success out of %d in %v", successes.Load(), total, time.Since(now))
 }
+
+func TestAdds(t *testing.T) {
+	if os.Getuid() != 0 {
+		t.Fatalf("Test requires root privileges.")
+	}
+
+	now := time.Now()
+	total := 300
+	tempDir, err := ioutil.TempDir("", "temp")
+	if err != nil {
+		t.Errorf("create tempDir: %v", err)
+	}
+	t.Logf("logs on %s", tempDir)
+	dbDir = tempDir
+	t.Setenv("CNI_LOG_FILE", filepath.Join(tempDir, "test.log"))
+	t.Cleanup(func() { os.RemoveAll(tempDir) })
+
+	// initialize variables
+	err = start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Save the current network namespace
+	origns, err := netns.Get()
+	if err != nil {
+		t.Fatalf("unexpected error trying to get namespace: %v", err)
+	}
+	defer origns.Close()
+
+	var successes atomic.Uint64
+	var wg sync.WaitGroup
+	argsCh := make(chan *skel.CmdArgs, total)
+	for i := 0; i < total; i++ {
+
+		runtime.LockOSThread()
+		rndString := make([]byte, 4)
+		_, err := rand.Read(rndString)
+		if err != nil {
+			t.Errorf("fail to generate random name: %v", err)
+		}
+		nsName := fmt.Sprintf("ns%x", rndString)
+		_, err = netns.NewNamed(nsName)
+		if err != nil {
+			t.Errorf("Failed to create network namespace: %v", err)
+		}
+		// Switch back to the original namespace
+		netns.Set(origns)
+		runtime.UnlockOSThread()
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			success := true
+			// Prepare CNI configuration
+			cniConfig := KindnetConf{
+				NetConf: types.NetConf{
+					CNIVersion: "0.3.1",
+					Name:       "test-network",
+					Type:       "cni-kindnet",
+				},
+				Ranges: []string{"fd00:1:2:3::/64"},
+			}
+
+			data, err := json.Marshal(cniConfig)
+			if err != nil {
+				success = false
+				t.Errorf("Failed to serialize cni config: %v", err)
+			}
+			//  Prepare CNI arguments
+			args := &skel.CmdArgs{
+				ContainerID: nsName,
+				Netns:       filepath.Join("/run/netns/", nsName),
+				IfName:      "eth0",
+				StdinData:   data,
+			}
+
+			//  Execute ADD command
+			if err := cmdAdd(args); err != nil {
+				success = false
+				t.Errorf("CNI ADD command failed: %v", err)
+			}
+
+			if success {
+				successes.Add(1)
+			}
+			argsCh <- args
+		}()
+	}
+	// database should be empty at this point
+	wg.Wait()
+	t.Logf("%d success added out of %d in %v", successes.Load(), total, time.Since(now))
+
+	for i := 0; i < total; i++ {
+		args := <-argsCh
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			//  Execute DEL command
+			if err := cmdDel(args); err != nil {
+				t.Errorf("CNI DEL command failed: %v", err)
+			}
+			netns.DeleteNamed(filepath.Base(args.Netns))
+		}()
+	}
+
+	wg.Wait()
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM pods").Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if the count is zero
+	if count != 0 {
+		t.Errorf("The pods database is not empty: %d", count)
+	} else {
+		t.Logf("no pods remaining in the db")
+	}
+	t.Logf("%d success out of %d in %v", successes.Load(), total, time.Since(now))
+}
