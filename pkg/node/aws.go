6c56c7993ca824a35d6ad66dd3c25741c851e4d5
Ciprian Hacman <ciprian@hakman.dev>
2025-01-03 13:13:34 +0200

    switch to aws-sdk-go-v2

--- pkg/node/aws.go
+++ pkg/node/aws.go
@@ -3,41 +3,65 @@
 package node
 
 import (
+	"context"
 	"fmt"
+	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
+func getAWSInstanceID(ctx context.Context, client *imds.Client) (string, error) {
+	identity, err := client.GetInstanceIdentityDocument(ctx, nil)
+	if err != nil {
+		return "", err
+	}
+	return identity.InstanceID, nil
+}
+
+func getAWSRegion(ctx context.Context, client *imds.Client) (string, error) {
+	region, err := client.GetRegion(ctx, nil)
+	if err != nil {
+		return "", err
+	}
+	return region.Region, nil
+}
+
 func disableAWSSrcDstCheck() error {
-	sess, err := session.NewSession()
-	if err != nil {
-		return fmt.Errorf("fail to create session: %w", err)
-	}
-	metadataClient := ec2metadata.New(sess)
-	region, err := metadataClient.Region()
-	if err != nil {
-		return fmt.Errorf("fail to get EC2 region name: %v", err)
-	}
-	sess.Config.Region = aws.String(region)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-	instanceID, err := metadataClient.GetMetadata("instance-id")
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		return fmt.Errorf("fail to get EC2 instance ID: %v", err)
+		return fmt.Errorf("fail to load AWS config: %w", err)
 	}
 
-	// Create new EC2 client
-	client := ec2.New(sess)
+	imdsClient := imds.NewFromConfig(cfg)
+	region, err := getAWSRegion(ctx, imdsClient)
+	if err != nil {
+		return fmt.Errorf("fail to get EC2 region name: %w", err)
+	}
+	instanceID, err := getAWSInstanceID(ctx, imdsClient)
+	if err != nil {
+		return fmt.Errorf("fail to get EC2 instance ID: %w", err)
+	}
+
+	ec2Client := ec2.NewFromConfig(cfg, func(o *ec2.Options) {
+		o.Region = region
+	})
 	attr := &ec2.ModifyInstanceAttributeInput{
-		InstanceId:      &instanceID,
-		SourceDestCheck: &ec2.AttributeBooleanValue{Value: aws.Bool(false)},
+		InstanceId: aws.String(instanceID),
+		SourceDestCheck: &types.AttributeBooleanValue{
+			Value: aws.Bool(false),
+		},
+	}
+	_, err = ec2Client.ModifyInstanceAttribute(ctx, attr)
+	if err != nil {
+		return fmt.Errorf("fail to disable source/destination check on AWS instance %s: %w", instanceID, err)
 	}
 
-	_, err = client.ModifyInstanceAttribute(attr)
-	if err != nil {
-		return fmt.Errorf("fail to disable src check on AWS instance %s: %v", instanceID, err)
-	}
 	return nil
 }
