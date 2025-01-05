085c8747d5ba756643562b9f8ec10909712c94e3
Antonio Ojea <aojea@google.com>
2025-01-05 11:50:47 +0000

    migrate nat64 to netlink nftables

---
+++ pkg/nat64/proxy_test.go
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: APACHE-2.0
+
+package nat64
+
+import (
+	"context"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/vishvananda/netns"
+)
+
+func TestNAT64Agent_SyncRules(t *testing.T) {
+	if os.Getuid() != 0 {
+		t.Skip("Test requires root privileges.")
+	}
+
+	tests := []struct {
+		name             string
+		expectedNftables string
+	}{
+		{
+			name: "simple",
+			expectedNftables: `table ip6 kindnet-nat64 {
+        chain prerouting {
+                type filter hook prerouting priority raw - 10; policy accept;
+                meta mark 0x0000000e return
+                ip6 daddr 64:ff9b::/96 meta l4proto udp tproxy to [::1]:60693 meta mark set 0x0000000d notrack accept
+                ip6 daddr 64:ff9b::/96 meta l4proto tcp tproxy to [::1]:45217 meta mark set 0x0000000d notrack accept
+                meta mark 0x0000000d drop
+        }
+}
+`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &NAT64Agent{
+				udpProxyPort: 60693,
+				tcpProxyPort: 45217,
+			}
+			runtime.LockOSThread()
+			defer runtime.UnlockOSThread()
+
+			// Save the current network namespace
+			origns, err := netns.Get()
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer origns.Close()
+
+			// Create a new network namespace
+			newns, err := netns.New()
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer newns.Close()
+
+			if err := n.SyncRules(context.Background()); err != nil {
+				t.Fatalf("NAT64Agent.SyncRules() error = %v", err)
+			}
+
+			cmd := exec.Command("nft", "list", "table", "ip6", tableName)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatalf("nft list table error = %v", err)
+			}
+			got := string(out)
+			if !compareMultilineStringsIgnoreIndentation(got, tt.expectedNftables) {
+				t.Errorf("Got:\n%s\nExpected:\n%s\nDiff:\n%s", got, tt.expectedNftables, cmp.Diff(got, tt.expectedNftables))
+			}
+			n.CleanRules()
+			cmd = exec.Command("nft", "list", "table", "ip6", tableName)
+			out, err = cmd.CombinedOutput()
+			if err == nil {
+				t.Fatalf("nft list ruleset unexpected success")
+			}
+			if !strings.Contains(string(out), "No such file or directory") {
+				t.Errorf("unexpected error %v %s", err, string(out))
+			}
+			// Switch back to the original namespace
+			netns.Set(origns)
+		})
+	}
+}
+
+func compareMultilineStringsIgnoreIndentation(str1, str2 string) bool {
+	// Remove all indentation from both strings
+	re := regexp.MustCompile(`(?m)^\s+`)
+	str1 = re.ReplaceAllString(str1, "")
+	str2 = re.ReplaceAllString(str2, "")
+
+	return str1 == str2
+}
