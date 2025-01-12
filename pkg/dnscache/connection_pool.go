8591ac4be54bb9ced535fb7ffa23b1af31170f39
Antonio Ojea <aojea@google.com>
2025-01-12 00:51:43 +0000

    connection pool

---
+++ pkg/dnscache/connection_pool.go
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: APACHE-2.0
+
+package dnscache
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+// https://www.rfc-editor.org/rfc/rfc7766
+// DNS Transport over TCP - Implementation Requirements
+const (
+	capacity = 3
+	timeout  = 50 * time.Millisecond
+)
+
+type Pools struct {
+	mu   sync.Mutex
+	pool map[string]*ConnectionPool
+}
+
+func NewPools() *Pools {
+	return &Pools{
+		pool: map[string]*ConnectionPool{},
+	}
+}
+
+func (d *Pools) Get(address string) *ConnectionPool {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	p, ok := d.pool[address]
+	if !ok {
+		p = NewConnectionPool(address)
+	}
+
+	return p
+}
+
+func (d *Pools) Delete(address string) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	p, ok := d.pool[address]
+	if !ok {
+		return
+	}
+	p.Close()
+	delete(d.pool, address)
+}
+
+// ConnectionPool manages a pool of TCP connections.
+type ConnectionPool struct {
+	mu          sync.Mutex
+	conns       chan *net.TCPConn
+	address     string
+	activeConns int
+}
+
+// NewConnectionPool creates a new connection pool.
+func NewConnectionPool(address string) *ConnectionPool {
+	pool := &ConnectionPool{
+		address: address,
+		conns:   make(chan *net.TCPConn, capacity),
+	}
+	return pool
+}
+
+// Get returns a connection from the pool for the given address.
+func (p *ConnectionPool) Get() (*net.TCPConn, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if p.activeConns < capacity {
+		conn, err := p.newConnection(p.address)
+		if err != nil {
+			return nil, err
+		}
+		p.activeConns++
+		return conn, nil
+	}
+
+	select {
+	case conn := <-p.conns:
+		return conn, nil
+	case <-time.After(timeout):
+	}
+
+	return nil, fmt.Errorf("no connections available")
+}
+
+// Put returns a connection to the pool for the given address.
+func (p *ConnectionPool) Put(conn *net.TCPConn) {
+	select {
+	case p.conns <- conn:
+	case <-time.After(timeout):
+		p.mu.Lock()
+		defer p.mu.Unlock()
+		p.activeConns--
+		conn.Close()
+	}
+}
+
+// newConnection establishes a new TCP connection to the given address.
+func (p *ConnectionPool) newConnection(address string) (*net.TCPConn, error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.SetKeepAlive(true)
+	conn.SetKeepAlivePeriod(30 * time.Second)
+
+	return conn, nil
+}
+
+// Close closes all connections in the pool.
+func (p *ConnectionPool) Close() {
+	for conn := range p.conns {
+		conn.Close()
+	}
+}
---
+++ pkg/dnscache/connection_pool_test.go
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: APACHE-2.0
+
+package dnscache
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestConnectionPoolIntegration(t *testing.T) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("failed to create listener, %v", err)
+	}
+	defer listener.Close()
+
+	var numConnections atomic.Int32
+	var numRequest atomic.Int32
+
+	go func() {
+		// listen for new connections
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				return
+			}
+			numConnections.Add(1)
+			// pass an accepted connection to a handler goroutine
+			go func() {
+				reader := bufio.NewReader(conn)
+				for {
+					// read client request data
+					msg, err := reader.ReadString('\n')
+					if err != nil {
+						if err != io.EOF {
+							t.Errorf("failed to read data, %v", err)
+						}
+						t.Logf("msg error %v", err)
+						return
+					}
+					numRequest.Add(1)
+					// prepend prefix and send as response
+					line := fmt.Sprintf("pong %s\n", msg)
+					conn.Write([]byte(line))
+				}
+			}()
+		}
+	}()
+
+	// Create a connection pool with a small capacity
+	pools := NewPools()
+	pool := pools.Get(listener.Addr().String())
+
+	// Send multiple DNS requests
+	var wg sync.WaitGroup
+	total := 5000
+	start := time.Now()
+	for i := 0; i < total; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			c, err := pool.Get()
+			if err != nil {
+				t.Errorf("Error getting connection: %v", err)
+				return
+			}
+			defer pool.Put(c)
+
+			c.Write([]byte("ping\n"))
+
+			reader := bufio.NewReader(c)
+			msg, err := reader.ReadString('\n')
+			if err != nil {
+				if err != io.EOF {
+					t.Errorf("failed to read data, %v", err)
+				}
+				return
+			}
+			if !strings.Contains(msg, "pong") {
+				t.Errorf("unexpected message: %s", msg)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if int(numConnections.Load()) != capacity {
+		t.Errorf("Expected %d connections to the test server, got %d", capacity, numConnections.Load())
+	}
+
+	if int(numRequest.Load()) != total {
+		t.Errorf("Expected %d requests to the test server, got %d", total, numRequest.Load())
+	}
+
+	t.Logf("%d requests with %d connections in %v", total, capacity, time.Since(start))
+}
